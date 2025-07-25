4252f3b7e3b8f71cff50fc1ab536aa546aebd404
Mata00616 <63013364+MatamandoKalilani@users.noreply.github.com>
2025-07-25 15:54:54 +0200

    save

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
+      <div className="flex flex-col items-center justify-center"></div>
+    </div>
+  );
+}
