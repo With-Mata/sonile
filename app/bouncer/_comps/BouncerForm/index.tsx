040e6cb04bd512821b9499e0e7b8af9fbc4d2a6a
Mata00616 <63013364+MatamandoKalilani@users.noreply.github.com>
2025-08-17 17:25:34 +0400

    delays

--- app/bouncer/_comps/BouncerForm/index.tsx
+++ app/bouncer/_comps/BouncerForm/index.tsx
@@ -65,7 +65,7 @@ const BouncerForm = ({ className, ...props }: BouncerFormProps) => {
         className="rounded-full disabled:opacity-50 disabled:pointer-events-none hover:bg-orange-400 inline-flex items-center justify-center gap-2.5 mt-8 w-max mx-auto min-w-52 bg-orange-300 focus:bg-orange-400 ring-orange-300 focus:ring-2 focus:ring-offset-2 px-4 py-2 text-black"
         onClick={(e) => {
           e.preventDefault();
-          handleSubmit(password);
+          handleSubmit(password.toUpperCase());
         }}
       >
         Let Me In
