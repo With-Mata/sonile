cf2d7059b7691982c95f067eab626d0530081dbd
Mata00616 <63013364+MatamandoKalilani@users.noreply.github.com>
2025-09-23 01:12:08 +0400

    Update app configuration to reflect new name and description for improved clarity

--- config/app-config.ts
+++ config/app-config.ts
@@ -1,6 +1,6 @@
 const appConfig = {
-  name: "Messages to Mata & Sonile 💌",
-  description: "Messages from Mata & Sonile.",
+  name: "Messages to Sonile",
+  description: "Messages from Mata to Sonile.",
   url: "https://sonile.withmata.dev",
   imageUrl: "https://sonile.withmata.dev/images/social-card-image.png",
 };
